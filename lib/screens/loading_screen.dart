d830ecd8fdb04ed4da6a48e81ff95293413815cf
Blari <Blarior@gmail.com>
2021-01-22 14:53:37 +0300

    Add api get data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,14 +9,38 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  void getLocation() async {
+    Location location = new Location();
+    await location.getCurrentLocation();
+  }
+
+  void getData() async {
+    http.Response res = await http.get(
+        'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=a67c348e50fc2db380ee36e1219db498');
+
+    if (res.statusCode == 200) {
+      String data = res.body;
+      print(data);
+    } else {
+      print(res.statusCode);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('code'),
+          ],
         ),
       ),
     );
